50a00e7250aa7820319b9907f22c9c5e33515b9c
marcelo-schreiber-dinf <marceloschreiber@ufpr.br>
2023-05-18 17:33:57 -0300

    Add: comments

--- src/insert.h
+++ src/insert.h
@@ -1,19 +1,23 @@
-
+// https://wiki.inf.ufpr.br/maziero/doku.php?id=pua:operacoes_em_diretorios
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <time.h>
+// Uma abordagem interessante consiste em colocar a área de diretório no final do archive, mantendo no início do deste apenas um inteiro indicando a posição do archive onde inicia essa área.
+// Essa organização torna mais simples as operações de alteração do conteúdo do arquivo
 
-typedef struct header
+typedef struct file
 {
     char *name;
     uid_t uid;
     mode_t permissions;
     off_t size;
     time_t date;
-    unsigned int order; // ordem no arquivo
+    unsigned int order;
     char *location;
-} header_t;
 
-header_t *insert_header(char *location, char *filename, unsigned int order);
-void print_header(header_t *header);
+} file_metadata;
+
+file_metadata *initialize_header();
+file_metadata *insert_header(char *location, char *filename, unsigned int order);
+void print_header(file_metadata *header);
