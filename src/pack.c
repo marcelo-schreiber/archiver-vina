ac44f21150da1961d91fcaeedbac640ec902b39d
marcelo-schreiber <maraugrissch@gmail.com>
2023-06-02 00:32:56 -0300

    Refac: add comment & remove printf

--- src/pack.c
+++ src/pack.c
@@ -30,11 +30,7 @@ void read_files_contents_and_write_to_archiver(unsigned int files_count, char **
     int size = 0;
 
     while ((size = fread(buffer, sizeof(char), MAX_BUF_SIZE, file)) > 0)
-    {
-
       fwrite(buffer, sizeof(char), size, archiver);
-      printf("buffer: %s\n", buffer);
-    }
 
     fclose(file);
     free(buffer);
@@ -57,18 +53,24 @@ void new_pack(char *vina_filename, char *file_names_with_path[], unsigned int fi
 
   for (unsigned int i = 0; i < files_count; i++)
   {
-    char *current_file_with_path = file_names_with_path[i];
-    char *current_file_name = strrchr(current_file_with_path, '/') + 1;
+    char *curr_file_with_path = file_names_with_path[i];
 
-    // get only the path
-    unsigned int rest_of_size = strlen(current_file_with_path) - strlen(current_file_name);
-    char *path = malloc(sizeof(char) * (rest_of_size + 1));
-    memset(path, 0, sizeof(char) * rest_of_size + 1);
-    strncpy(path, current_file_with_path, rest_of_size);
-    path[rest_of_size] = '\0';
+    // ./pasta/new_LEIAME.md -> new_LEIAME.md
+    char *current_file_name = strrchr(curr_file_with_path, '/') + 1;
 
-    insert_header(header, path, current_file_name, i + 1); // TODO: get file path from command line
+    // ./pasta/new_LEIAME.md -> ./pasta/
+    unsigned int only_path_size = strlen(curr_file_with_path) - strlen(current_file_name);
+
+    char *path = malloc(sizeof(char) * (only_path_size + 1)); // +1 for '\0'
+    memset(path, 0, sizeof(char) * (only_path_size + 1));
+
+    strncpy(path, curr_file_with_path, only_path_size); // copy only path to path variable
+    path[only_path_size] = '\0';                        // add '\0' to end of path variable
+
+    insert_header(header, path, current_file_name, i + 1);
     fwrite(header, sizeof(file_metadata), 1, archiver);
+
+    free(path);
   }
 
   read_files_contents_and_write_to_archiver(files_count, file_names_with_path, archiver);
